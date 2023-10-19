ec2b6cb2c66945008e101f68b88551be289df429
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-10-19 16:39:51 +0100

    Interests section complete

--- react-first-project/src/components/BusinessCard.jsx
+++ react-first-project/src/components/BusinessCard.jsx
@@ -1,5 +1,6 @@
 import Main from "../components/Main"
 import About from "../components/About"
+import Interests from "../components/Interests"
 import style from "./BusinessCard.module.css"
 
 
@@ -8,6 +9,7 @@ const BusinessCard = () => {
     <div className={style.main_container}>
       <Main />
       <About />
+      <Interests />
     </div>
   )
 }
--- react-first-project/src/components/Interests.jsx
+++ react-first-project/src/components/Interests.jsx
@@ -1,8 +1,15 @@
-
+import style from "./Interests.module.css"
 
 const Interests = () => {
   return (
-    <div>Interests</div>
+    <div>
+      <h3 className={style.title}>Interests</h3>
+      <p className={style.about_txt}>
+        Foodie. Music lover. Internet
+        fanatic. Bacon buff. Entrepreneur. Travel geek.
+        Coffee fanatic.
+      </p>
+    </div>
   )
 }
 
